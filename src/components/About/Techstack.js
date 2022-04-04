3dedfe83512290bc35318a278147c791ca8e42c9
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-04-04 18:44:13 +0530

    projects updated

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -8,62 +8,63 @@ import {
   DiHtml5,
   DiCss3,
   DiNpm,
-  DiGit
+  DiGit,
 } from "react-icons/di";
 
-import { SiRedux , SiRedis} from "react-icons/si";
+import { SiRedux, SiRedis } from "react-icons/si";
 
 function Techstack() {
   return (
-    <Row id="skills" style={{ justifyContent: "center", paddingBottom: "50px" }}>
-     
+    <Row
+      id="skills"
+      style={{ justifyContent: "center", paddingBottom: "50px" }}
+    >
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript />
-        <h3>JavaScript</h3> 
+        <h3>JavaScript</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-           <DiHtml5 />
-           <h3>HTML</h3> 
+        <DiHtml5 />
+        <h3>HTML</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiCss3 />
-        <h3>CSS</h3> 
+        <h3>CSS</h3>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
-        <h3>Node.js</h3> 
+        <h3>Node.js</h3>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <SiRedis />
-        <h3>Redis</h3> 
+        <h3>Redis</h3>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
-        <h3>React</h3> 
+        <h3>React</h3>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <SiRedux />
-        <h3>Redux</h3> 
+        <h3>Redux</h3>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
-        <h3>MongoDb</h3> 
+        <h3>MongoDb</h3>
       </Col>
-     
+
       <Col xs={4} md={2} className="tech-icons">
         <DiNpm />
-        <h3>NPM</h3> 
+        <h3>NPM</h3>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
-        <h3>Git</h3> 
-      </Col> 
-
+        <h3>Git</h3>
+      </Col>
     </Row>
   );
 }
