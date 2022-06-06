80b105f943abd405eb1579d5c6ea1b7e916b38e9
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-06-06 22:10:34 +0530

    projects updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -31,12 +31,12 @@ function Projects() {
               isDeploy={true}
               isAblog={true}
               title="Soundcloud"
-              description="SoundCloud is a online audio distribution platform and music sharing website that enables its users to upload, promote, and share audio, as well as a digital signal processor enabling listeners to stream audio."
+              description="SoundCloud is an online audio distribution platform and music sharing website that enables its users to upload, promote, and share audio, as well as a digital signal processor enabling listeners to stream audio."
               links="https://web-jinke-music.vercel.app/"
               link="https://github.com/BhaveshSuthar00/soundCld-client-side"
               // blog="https://medium.com"
-              techstack="TECH-STACKS : ReactJs, mongoDb, Express.js, Node.js, react-jinke-music-player"
-              storage="DATABASE : MongoDb Atlas & Served by Heroku."
+              techstack="TECH-STACKS : ReactJs, MongoDB, Express.js, Node.js, react-jinke-music-player"
+              storage="DATABASE: MongoDB Atlas & Served by Heroku."
 
             />
           </Col>
@@ -51,9 +51,8 @@ function Projects() {
               link="https://github.com/jeevan243/flatheads"
               links="https://flatheads-official.herokuapp.com/"
               blog="https://medium.com/@reddymanish809/flatheads-clone-unit-4-4056d57a664e"
-              techstack="TECH-STACKS: EJS, CSS, mongoDb, ES6 JS, Express.js, Node.js."
-              storage="DATABASE : MongoDb Atlas"
-
+              techstack="TECH-STACKS: EJS, CSS, MongoDB, ES6 JS, Express.js, Node.js."
+              storage="DATABASE: MongoDB Atlas"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -65,7 +64,7 @@ function Projects() {
               title="American Eagle"
               description="It is an e-commerce web application that allows users to buy men's and women's clothing online. We were a five-person team that worked on this project for five days. The Landing Page is my contribution to this project. We have built a complete e-commerce flow."
               link="https://github.com/jeevan243/American-Eagle-Web"
-              links="https://jeevan243.github.io/American-Eagle-Web/index.html"
+              links="https://american-eagle-web-clone.vercel.app/"
               blog="https://medium.com/@jeevanprasad243/our-project-american-eagle-clone-f5e2894b57d4"
               techstack="TECH-STACKS : HTML, CSS, DOM, JavaScript"
               storage="DATABASE : Local Storage"
